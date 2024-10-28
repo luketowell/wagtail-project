3c458cec1463e84a69f5d4cb581698ae53ba3e39
Luke Towell <luke.towell@burendo.com>
2024-10-28 16:58:18 +0000

    added a next js front end

---
+++ blog/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+from wagtail.models import Page
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel
+from wagtail.search import index
+
+# Create your models here.
+class BlogIndexPage(Page):
+    intro = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro')
+    ]
+
+    subpage_types = ["blog.BlogPage"]
+
+class BlogPage(Page):
+    date = models.DateField("Post Date")
+    intro = models.CharField(max_length=250)
+    body = RichTextField(blank=True)
+
+    search_fields = Page.search_fields + [
+        index.SearchField('intro'),
+        index.SearchField('body')
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel('date'),
+        FieldPanel('intro'),
+        FieldPanel('body')
+    ]
+
+    parent_page_types = ["blog.BlogIndexPage"]
---
+++ wagtailproject/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-ceg(2l$qzj1u-t5rvdd4$tf1c+-^4x-5awh@4a!-*h4f*=injp"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
