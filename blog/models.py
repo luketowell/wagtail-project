2dcdc5c0946984d38692dd6f6b555a6ce41d38ce
Luke Towell <luketowell@Lukes-MacBook-Pro.local>
2024-10-29 20:22:06 +0000

    Wired up the wagtail api functionality in the blog models.py

--- blog/models.py
+++ blog/models.py
@@ -4,10 +4,15 @@ from wagtail.models import Page
 from wagtail.fields import RichTextField
 from wagtail.admin.panels import FieldPanel
 from wagtail.search import index
+from wagtail.api import APIField
 
 # Create your models here.
 class BlogIndexPage(Page):
     intro = RichTextField(blank=True)
+
+    api_fields = [
+        APIField('intro')
+    ]
 
     content_panels = Page.content_panels + [
         FieldPanel('intro')
@@ -19,6 +24,12 @@ class BlogPage(Page):
     date = models.DateField("Post Date")
     intro = models.CharField(max_length=250)
     body = RichTextField(blank=True)
+
+    api_fields = [
+        APIField('date'),
+        APIField('intro'),
+        APIField('body')
+    ]
 
     search_fields = Page.search_fields + [
         index.SearchField('intro'),
--- wagtailproject/api.py
+++ wagtailproject/api.py
@@ -6,4 +6,5 @@ from wagtail.documents.api.v2.views import DocumentsAPIViewSet
 api_router = WagtailAPIRouter('wagtailapi')
 
 api_router.register_endpoint('pages', PagesAPIViewSet)
+api_router.register_endpoint('images', ImagesAPIViewSet)
 api_router.register_endpoint('documents', DocumentsAPIViewSet)
